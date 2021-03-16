2d51ca55b325ee43f0689afffd60e09a623a7793
amin1014 <aminabdi570@gmail.com>
2021-03-16 11:47:44 +0300

    add project files

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-$("#designimg").click(function() {
-    $(this).hide();
-    $("#designtxt").removeClass("designtxt");
+$(document).ready(function() {
+
+
+  $("form#mc-embedded-subscribe-form").submit(function(event) {
+  event.preventDefault();
+    let name = $("input#mce-FNAME").val();
+    let email = $("input#mce-EMAIL").val();
+    let message = $("textarea").val();
+
+
+    if ( name && email && message){
+          alert (`Hi ${name} we have received your message. Thank you for reaching out to us.`);
+      }
+    else {
+      alert("Please enter your name and email!");
+        }
   });
-  $("#devimg").click(function() {
-    $(this).hide();
-    $("#devtxt").removeClass("devtxt");
-  });
+
   
-  $("#prodimg").click(function() {
-    $(this).hide();
-    $("#prodtxt").removeClass("prodtxt");
-  });
+  let cardTotalNumber =(
+      $(".card").toArray().length
+    );
+
+  for(let cardNumber = 1; cardNumber<=cardTotalNumber;cardNumber++ ){
+      $(`.card${cardNumber}`).click(function() {
+          $(`.card-show${cardNumber}`).toggle(400);
+          $(`.card-hidden${cardNumber}`).toggle(400);
+        });
+    }
   
-  $("#designtxt").click(function() {
-    $("#designimg").show();
-    $("#designtxt").addClass("designtxt");
+
+    let overlayTotalNumber =(
+      $(".overlay").toArray().length
+    );
+
+    for(let overlayNumber = 1; overlayNumber<=overlayTotalNumber;overlayNumber++){
+        $(`#overlay${overlayNumber}`).mouseover(function() {
+        $(`#overlay${overlayNumber} > img`).addClass("overlay-effect");
+        $(`#work-overlay${overlayNumber}`).toggle();
+      }).mouseout(function(){
+        $(`#overlay${overlayNumber} > img`).removeClass("overlay-effect");
+        $(`#work-overlay${overlayNumber}`).toggle();
+      });
+    }  
   });
-  
-  $("#devtxt").click(function() {
-    $("#devimg").show();
-    $("#devtxt").addClass("devtxt");
-  });
-  
-  $("#prodtxt").click(function() {
-    $("#prodimg").show();
-    $("#prodtxt").addClass("prodtxt");
-  });
-  
-  $("#pro2").hover(function() {
-      console.log("hovered")
-      $("this").addClass("mask");
-  });
-  
